ce233f7a6fb2a64b1948f5192f91f9b0f8cf609f
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2024-07-08 21:59:12 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import yfinance
+import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
@@ -16,13 +16,7 @@ with st.sidebar:
     st.write("-------------------------------------")
     search_date = st.radio('select time?',["D","H","30M"],horizontal=True)
     if search_date == "D":
-        df = yfinance.download(tickers = 'EURUSD=X' ,start="2023-02-01" ,end=end)
-        model = tf.keras.models.load_model("final2_model_EUR_USD(1).h5" , custom_objects=None, compile=True, safe_mode=True)
-    elif search_date == "H":
-        df = yf.download(tickers = 'EURUSD=X' ,start=start ,end=end , interval="1h")
-        model = tf.keras.models.load_model("final2_model_EUR_USD(1).h5" , custom_objects=None, compile=True, safe_mode=True)
-    elif search_date == "30M":
-        df = yf.download(tickers = 'EURUSD=X' ,start=start,end=end , interval="30D")
+        df = yf.download(tickers = 'EURUSD=X' ,start="2023-02-01" ,end=end)
         model = tf.keras.models.load_model("final2_model_EUR_USD(1).h5" , custom_objects=None, compile=True, safe_mode=True)
     st.write("-------------------------------------")    
     
